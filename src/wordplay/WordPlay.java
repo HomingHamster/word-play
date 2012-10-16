f4037e35abb30ae96d6ea87ffb1d9f2d9a04bb60
Felix Farquharson <felix.farquharson@googlemail.com>
2012-10-16 12:31:58 +0100

    Created netbeans project, created main java file. Added dictionaries and writeup tex file. Added .gitignore (which is included in the repository so others can edit. added .gitignore to

---
+++ src/wordplay/WordPlay.java
@@ -0,0 +1,16 @@
+package wordplay;
+
+/**
+ *
+ * @author Felix Farquharson
+ */
+public class WordPlay {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
