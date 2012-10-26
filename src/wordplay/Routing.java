a2a44f2c60ab28519344f361c7b8d91e5f3288a7
Felix Farquharson <felix.farquharson@googlemail.com>
2012-10-26 05:04:43 +0100

    added a route finding class now playing with implementing the algos that are finding the shortest route.

---
+++ src/wordplay/Routing.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wordplay;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+/**
+ *
+ * @author felix
+ */
+public class Routing {
+    public static ArrayList<String> findRoute(String word, Hashtable dict){
+        ArrayList<String> route = new ArrayList<String>();
+        
+        
+        return route;
+    }
+    
+}
