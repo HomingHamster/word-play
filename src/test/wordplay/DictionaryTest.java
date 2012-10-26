d159e75a74c6bc7438292935668168b39dfbc274
Felix Farquharson <felix.farquharson@googlemail.com>
2012-10-26 05:00:48 +0100

    added junit tests, no real logic there yet. modified .gitignore.

---
+++ src/test/wordplay/DictionaryTest.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.wordplay;
+
+import java.util.Hashtable;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import wordplay.Dictionary;
+
+/**
+ *
+ * @author felix
+ */
+public class DictionaryTest {
+    
+    public DictionaryTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of readDict method, of class Dictionary.
+     */
+    @Test
+    public void testReadDict() {
+        System.out.println("readDict");
+        String fileName = "/home/felix/word-play/dictionaries/test-6-word.txt";
+        Dictionary instance = new Dictionary();
+        Hashtable expResult = null;
+        Hashtable result = instance.readDict(fileName);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getDict method, of class Dictionary.
+     */
+    @Test
+    public void testGetDict() {
+        System.out.println("getDict");
+        String dictName = "";
+        Dictionary instance = new Dictionary();
+        Hashtable expResult = null;
+        Hashtable result = instance.getDict(dictName);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
